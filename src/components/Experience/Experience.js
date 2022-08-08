6633ea114540161a0de1ef5c9937d29b8318f59f
arojasvargas <alex.rojas547@outlook.com>
2022-08-08 15:58:42 -0600

    minimized bottom padding in each component

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -4,9 +4,9 @@ import Styles from './Experience.module.css'
 
 const Experience = () => {
   return (
-    <div className={'container p-5 my-2 shadow rounded bg-light'}>
+    <div className={'container p-5 pb-4 my-2 shadow rounded bg-light'}>
         <h3>Experience</h3>
-        <ul className='ps-0 ps-lg-4'>
+        <ul className='ps-0 ps-lg-4 m-0'>
             {experience.map(
                 (item) => {
                   
